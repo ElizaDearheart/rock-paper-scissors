2c03648fafcc0ac4cff520699e4ff39000e4ded0
Jordan Keller <keller.jc@gmail.com>
2022-08-10 15:42:48 -0700

    Add conditional branches to playRound function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,29 +1,41 @@
-const handOptions = ["Rock", "Paper", "Scissors"];
+const handOptions = ["rock", "paper", "scissors"];
 
 //getComputerChoice 
 const getComputerChoice = Math.floor(Math.random() * handOptions.length);
 
-//computerSelection
-const computerSelection = handOptions[getComputerChoice].toLowerCase()
-console.log(computerSelection)
+//convert to computerSelection
+const computerSelection = handOptions[getComputerChoice].toLowerCase();
+console.log(computerSelection);
 
 //playerSelection
-let playerInput = prompt(`Please choose ${handOptions.join(", ")}.`)
-const playerSelection = playerInput.toLowerCase()
-console.log(playerSelection)
+let playerInput = prompt(`Please choose ${handOptions.join(", ")}.`);
+var playerSelection = playerInput.toLowerCase();
+console.log(playerSelection);
 
 //playRound(playerSelection, computerSelection), 
 //returns the winner of the round and what beat what.
+let getComputerChoiceIndex = handOptions.indexOf(computerSelection);
+let getPlayerSelectionIndex = (handOptions.indexOf(playerSelection));
+let getAdjustedPlayerSelectionIndex = (handOptions.indexOf(playerSelection)) - 1;
+
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection)
-        return alert("Game tied!")
+    if (playerSelection === computerSelection) {
+        console.log("Game tied!");
+    } else if (getAdjustedPlayerSelectionIndex === getComputerChoiceIndex) {
+        console.log("You win.");
+    } else if (getPlayerSelectionIndex === 0 && getComputerChoiceIndex === 2) {
+        console.log("You win.")
+    } else {
+        console.log("You lose.");
+    }
 
 };
+console.log(playRound(playerSelection, computerSelection));
+
 
 //game function plays five rounds of playRound
 ///for (let = 0; i < 5; i++)
 
-
-
-//
-
+console.log(getComputerChoiceIndex);
+console.log(getPlayerSelectionIndex);
+console.log(getAdjustedPlayerSelectionIndex);
