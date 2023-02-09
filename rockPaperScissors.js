7c604cfaa0d05906927ce2ad93eb2abc4697edc9
Jordan Keller <keller.jc@gmail.com>
2023-02-09 12:16:07 -0800

    Add score keepingto page

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,12 +1,35 @@
 let playerChoice;
-
+// buttons//
 document.getElementById('rock').onclick = playRound;
 document.getElementById('paper').onclick = playRound;
 document.getElementById('scissors').onclick = playRound;
+//divs//
+let container = document.querySelector('#container');
+//total score
+let total_score = document.createElement('div');
+total_score.classList.add('total_score');
+container.appendChild(total_score);
+//player score
+let player_score = document.createElement('div');
+total_score.classList.add('player_score');
+container.appendChild(player_score);
+//computer score
+let computer_score = document.createElement('div');
+computer_score.classList.add('computer_score');
+container.appendChild(computer_score);
+//player wins
+let player_wins = document.createElement('div');
+player_wins.classList.add('player_wins');
+container.appendChild(player_wins);
+//computer wins
+let computer_wins = document.createElement('div');
+computer_wins.classList.add('computer_wins');
+container.appendChild(computer_wins);
 
 let gameCount = 0;
 let playerWinCount = 0;
 let computerWinCount = 0;
+
 
 let computerHandOptions = ["rock", "paper", "scissors"];
 
@@ -19,6 +42,7 @@ function playRound() {
     let computerSelection = computerHandOptions[getComputerChoice];
 
     alert("Player: " + playerChoice);
+
     console.log(playerChoice)
     alert("Computer: " + computerSelection);
     console.log(computerSelection);
@@ -71,11 +95,17 @@ function playRound() {
             }
         }
     }
+    total_score.textContent = "Game total: " + gameCount;
+    player_score.textContent = "Player total: " + playerWinCount;
+    computer_score.textContent = "Computer total: " + computerWinCount;
+
     if (gameCount === 5) {
         gameCount = 0
         if (playerWinCount > computerWinCount) {
+            player_wins.textContent = "Player wins!  Congratulations!"
             alert('Game over. Player wins.')
         } else {
+            computer_wins.textContent = "Game over.  Computer wins."
             alert('Game over. Computer wins.')
         }
     }
